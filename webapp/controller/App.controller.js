d0d0c1968fe82fca34832bec49ddc0cf02544868
Raymond Tabares <ratara5@gmail.com>
2022-10-11 15:20:19 +0000

    Step 15 - Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,16 +5,5 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-		onShowHello : function () {
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-			// show message
-			MessageToast.show(sMsg);
-		}
 	});
-
 });
